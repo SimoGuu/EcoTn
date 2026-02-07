8f10647b59d057256488c7be91342ef3412cb8c0
Leonardo Essam <leonardoessam.deirossi@gmail.com>
2026-02-07 15:22:16 +0100

    Migliorato API handler

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,17 +1,29 @@
+const {request, response} = require("express");
+
 const WebApiController = {
+    sendResponse: (request, response, data, message) => {
+        response.json(
+            {
+                status: 200,
+                result: data,
+                message: message,
+                debug: null
+            }
+        );
+    },
     sendError: (request, response, status, error) => {
         response
             .status(status)
             .type("application/problem+json")
             .json(
-            {
-                type: error.type,
-                title: error.title,
-                status: status,
-                details: error.details,
-                instance: request.originalUrl
-            }
-        );
+                {
+                    type: error.type,
+                    title: error.title,
+                    status: status,
+                    details: error.details,
+                    instance: request.originalUrl
+                }
+            );
     }
 };
 
