2b3446c099081559880910004072a102ce193268
Leonardo Essam <leonardoessam.deirossi@gmail.com>
2026-02-07 15:22:46 +0100

    Endpoint per l'autenticazione SPID

--- src/index.js
+++ src/index.js
@@ -3,30 +3,29 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const WebApiController = require("./controllers/index");
+const SpidLoginController = require("./controllers/spidLogin");
 
 app.get("/", (request, response) => {
-   response.json(
-       {
-           status: 200,
-           result: null,
-           message: "Hello, World!",
-           debug: null
-       }
-   );
+    response.json({
+        status: 200, result: null, message: "Hello, World!", debug: null
+    });
+});
+
+app.get("/api/v1/spid/login", (request, response) => {
+    SpidLoginController.doLogin(request, response);
+});
+
+app.get("/api/v1/spid/callback", async (request, response) => {
+    await SpidLoginController.handleCallback(request, response);
 });
 
 app.use((request, response, next) => {
-    WebApiController.sendError(
-        request,
-        response,
-        404,
-        {
-            type: "not-found",
-            title: "Resource not found",
-            status: 404,
-            details: "The requested resource was not found on this server."
-        }
-    );
+    WebApiController.sendError(request, response, 404, {
+        type: "not-found",
+        title: "Resource not found",
+        status: 404,
+        details: "The requested resource was not found on this server."
+    });
 });
 
 app.listen(port, () => {
