0df763900fa98e23cc9bc7ef9fdcebbf82807fb4
Simone Guerra <simone.guerra-1@studenti.unitn.it>
2026-02-07 22:46:50 +0100

    implementato cambio tema

--- public/scripts/pagina-impianto.js
+++
@@ -1,7 +0,0 @@
-function gestisciPeriodo() {
-  console.log("Cambio");
-  const periodo = document.getElementById("periodo").value;
-  const custom = document.getElementById("customDates");
-
-  custom.style.display = periodo === "custom" ? "block" : "none";
-}
