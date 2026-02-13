bf6b4a0aad5dfbc6084bfa4d839fd96421060d42
Daniele L. <danilacarbi@icloud.com>
2026-02-13 19:56:03 +0100

    [minor] Configurazione Jest

--- src/app/models/consumption.js
+++ src/app/models/consumption.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const consumptionSchema = new Schema({
   abitazione: { type: Schema.Types.ObjectId, ref: 'House', required: true },
   orario: { type: Date, required: true },
-  valore: { type: Number, required: true }
+  valore: { type: Number, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Consumption', consumptionSchema);
