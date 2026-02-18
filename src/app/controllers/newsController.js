a1b1986264b161dc958719b631d08b29d2662955
Daniele L. <danilacarbi@icloud.com>
2026-02-18 14:46:51 +0100

    Agggiunta funzione: trova notizie da id chiave

--- src/app/controllers/newsController.js
+++ src/app/controllers/newsController.js
@@ -28,3 +28,12 @@ exports.getNewsById = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+exports.getNewsByKey = async (req, res) => {
+  try {
+    const news = await News.find({ chiavi: req.params.keyId });
+    res.status(200).json(news);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- src/app/routes/newsRoutes.js
+++ src/app/routes/newsRoutes.js
@@ -4,6 +4,5 @@ const newsController = require('../controllers/newsController');
 
 router.get('/', newsController.getNews);
 router.post('/', newsController.createNews);
-router.get('/:id', newsController.getNewsById);
 
 module.exports = router;
